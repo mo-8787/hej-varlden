daa01439d5d60a0fbb0ddb666267f4dfe5cd5988
moham <mohammedmohammadi63@gmail.com>
2026-09-08 20:11:24 +0200

    added line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hej Världen!");
+        System.out.println("Hej Världen123!");
     }
 }
