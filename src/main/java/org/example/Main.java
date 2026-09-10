e949c668831f87c59a43b62f974531f033943882
moham <mohammedmohammadi63@gmail.com>
2026-09-10 18:00:23 +0200

    2 new added lines

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hej Världen!");
-        System.out.println("Hej Världen123!");
+        System.out.println("Mitt namn är Mo");
+        System.out.println("Dagens datum är 2026-08-26");
     }
 }
