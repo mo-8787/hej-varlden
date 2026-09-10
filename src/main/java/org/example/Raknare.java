292b81b977644a42b1b079b3af2a8bd31fda05de
moham <mohammedmohammadi63@gmail.com>
2026-09-11 01:48:56 +0200

    Division

--- src/main/java/org/example/Raknare.java
+++ src/main/java/org/example/Raknare.java
@@ -9,7 +9,7 @@ public class Raknare {
         int a = Scan.nextInt();
         System.out.print("Ange ett ytterligare tal: ");
         int b = Scan.nextInt();
-        System.out.println("Produkten är: " + (a * b));
+        System.out.println("Kvoten är: " + (a / b));
 
     }
 }
