40623ac9b5baa8181b60d411ac9cdd5a57dace00
moham <mohammedmohammadi63@gmail.com>
2026-09-11 01:40:16 +0200

    Addition

---
+++ src/main/java/org/example/Raknare.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Raknare {
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+        System.out.print("Ange ett tal: ");
+        int a = Scan.nextInt();
+        System.out.print("Ange ett ytterligare tal: ");
+        int b = Scan.nextInt();
+        System.out.println("Summan är: "+ (a + b));
+
+
+    }
+}
