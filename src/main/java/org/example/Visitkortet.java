e48d2f9ddc1bf2e8d9f47667e7e23ac8bf924dab
moham <mohammedmohammadi63@gmail.com>
2026-09-12 01:24:37 +0200

    Visitkortet

---
+++ src/main/java/org/example/Visitkortet.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Visitkortet {
+    public static void main(String[] args) {
+        String name = "Esme";
+        int age = 23;
+        double height = 1.70;
+        boolean student = true;
+
+        System.out.println("Name is: "+ name + "\nAge is: " + age + "\nHeight is: " + height + " m \nStudent: " + student);
+
+    }
+}
