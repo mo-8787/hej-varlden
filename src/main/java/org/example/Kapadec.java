8bbb58c0669572973304c09dda4de5a3de2f42d2
moham <mohammedmohammadi63@gmail.com>
2026-09-12 03:11:04 +0200

    Kapa decimalerna

---
+++ src/main/java/org/example/Kapadec.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Kapadec {
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+        System.out.print("Enter a decimal number: ");
+        double a = Scan.nextDouble();
+        System.out.println("Heltalsdelen är: " + (int)a);
+
+
+    }
+}
