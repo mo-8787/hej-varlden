413f94849cb01c3d434b283a4ebf8e1b1cdf66cd
moham <mohammedmohammadi63@gmail.com>
2026-09-13 21:27:33 +0200

    If, else if, else add ons

--- src/main/java/org/example/BMIcalculator.java
+++ src/main/java/org/example/BMIcalculator.java
@@ -8,16 +8,32 @@ public class BMIcalculator {
         System.out.print("Please enter your name: ");
         String name = Scan.nextLine();
 
-        System.out.print("Please enter your weight: ");
+        System.out.print("Please enter your weight(kg): ");
         double weight = Scan.nextDouble();
+        Scan.nextLine();
 
-        System.out.print("Please enter your height: ");
-        double height = Scan.nextDouble();
+        System.out.print("Please enter your height(cm): ");
+        double heightCm = Scan.nextDouble();
+        Scan.nextLine();
+
+        double heightMeters = heightCm / 100.0;
 
         // BMI = weight / (height * height)
-        double BMI = weight / (height * height);
+        double BMI = weight / (heightMeters * heightMeters);
 
         System.out.printf("Hi %s, your BMI is %.1f%n" , name, BMI);
+
+        System.out.println("A healthy BMI is between 18.5 and 25.");
+
+        if (BMI < 18.5) {
+            System.out.println("You are underweight.");
+        } else if (BMI < 25) {
+            System.out.println("You are normal weight.");
+        } else if (BMI < 30) {
+            System.out.println("You are overweight.");
+        } else {
+            System.out.println("You are obese.");
+        }
 
         Scan.close();
 
