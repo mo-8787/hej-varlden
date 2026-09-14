6427c569e849190c7a8149d071b3f053e07906de
moham <mohammedmohammadi63@gmail.com>
2026-09-14 23:31:37 +0200

    GuessTheNumber

--- src/main/java/org/example/loops/GuessTheNumber.java
+++ src/main/java/org/example/loops/GuessTheNumber.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class GuessTheNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         Random random = new Random();
 
         int secret = random.nextInt(10) +1;
@@ -14,16 +13,17 @@ public class GuessTheNumber {
         int input;
         System.out.print("Guess the number: ");
         do {
-        input = scan.nextInt();
-        guesses++;
-        if (input < secret){
+            input = scan.nextInt();
+            guesses++;
+            if (input < secret){
                 System.out.print("Higher! Guess another number: ");
             } else if (input > secret) {
-            System.out.print("Lower! Guess another number: ");
+                System.out.print("Lower! Guess another number: ");
             }
         } while (input != secret);
         String Singleguess = guesses == 1 ? " guess." : " guesses.";
         System.out.println("Congrats you got it. You got it in " + guesses + Singleguess);
 
+        scan.close();
     }
 }
