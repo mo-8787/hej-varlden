29baccde254fd98ce21902df263bdef1ddb10037
moham <mohammedmohammadi63@gmail.com>
2026-09-14 03:55:32 +0200

    switch another example

--- src/main/java/org/example/ExempelSwitch.java
+++ src/main/java/org/example/ExempelSwitch.java
@@ -4,29 +4,18 @@ import java.util.Scanner;
 
 public class ExempelSwitch {
     public static void main(String[] args) {
-        System.out.print("Välj dag (1-7): ");
         Scanner Scan = new Scanner(System.in);
-        int day = Scan.nextInt();
-        String name = "";
-        switch (day) {
-            case 1: name ="monday";
-            break;
-            case 2: name ="tuesday";
-                break;
-            case 3: name ="wednesday";
-                break;
-            case 4: name ="thursday";
-                break;
-            case 5: name ="friday";
-                break;
-            case 6: name ="saturday";
-                break;
-            case 7: name ="sunday";
-                break;
-            default:
-                System.out.println("Va!?");
+        String a = "Hej";
+        System.out.print("Skriv \"Hej\": " );
+        String userInput = Scan.nextLine();
+        if(userInput.equalsIgnoreCase(a)){
+            System.out.println("Du skriver oerhört rätt!");
         }
-        System.out.println("Veckodag: " + name );
+        else System.out.println("Nu skrev du fel. ");
+
+
+
+
         Scan.close();
 
     }
