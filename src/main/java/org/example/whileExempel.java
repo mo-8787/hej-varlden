be4ba5b57a1b80c394d8511c1f82961726df132f
moham <mohammedmohammadi63@gmail.com>
2026-09-14 05:31:13 +0200

    switch another example, veckodag och helg

---
+++ src/main/java/org/example/whileExempel.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class whileExempel {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 5) {
+            System.out.println("Hej " + count);
+            count++;
+        }
+    }
+}
