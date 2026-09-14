5ee875354b876d6807032d4bf0f8b5f78faf7c02
moham <mohammedmohammadi63@gmail.com>
2026-09-14 05:31:24 +0200

    switch another example, veckodag och helg

---
+++ src/main/java/org/example/foreachExempel.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class foreachExempel {
+    public static void main(String[] args) {
+        String[]  names = {"Anna", "Bo", "Cecilia" };
+        for (String name : names) {
+            System.out.println("Hej " + name);
+        }
+    }
+}
